24306aafecaeed670ee2ddf7b41a4628ae7ab245
Saanvi Arora <arorasaanvi777@gmail.com>
2026-03-26 00:17:38 +0530

    page 2 make

--- GSAP Animations/custom cursor/script.js
+++ GSAP Animations/custom cursor/script.js
@@ -17,8 +17,44 @@ window.addEventListener("mousemove", function(dets){
     gsap.to(cursor,{
         x: dets.clientX,
         y: dets.clientY,
-        ease:"power2.out",
-        duration:0.3,
+        ease:"power4.out",
+        duration:0.2,
         scale:1.5,
     })
+})
+var headname = document.querySelector(".page2 h1");
+var arr=headname.textContent.split("");
+
+var flag="";
+var count=0;
+arr.forEach(function(elem){
+    if(count<=5){
+    flag+=`<span class="a">${elem}</span>`;}
+    else if(elem === " ") {
+        flag += `<span class="space"> </span>`;
+    }
+    else{
+        flag+=`<span class="b">${elem}</span>`;
+    }
+    count++;
+})
+headname.innerHTML=flag;
+gsap.from(".a",{
+    y: 100,
+    opacity:0,
+    ease:"bounce",
+    duration:2,
+    stagger:0.1,
+     scrollTrigger: {
+    trigger: ".page2 h1",
+    start: "top 80%",
+  }
+})
+gsap.from(".b",{
+    y: 100,
+    opacity:0,
+    ease:"bounce",
+    duration:2,
+    stagger:-0.1,
+    scrollTrigger:".b",
 })

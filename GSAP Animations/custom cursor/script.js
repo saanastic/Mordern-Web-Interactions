1431aac2a26ece4e86988213584e6be78ec59b56
Saanvi Arora <arorasaanvi777@gmail.com>
2026-03-25 05:48:42 +0530

    basic js add

--- GSAP Animations/custom cursor/script.js
+++ GSAP Animations/custom cursor/script.js
@@ -1,3 +1,21 @@
-gsap.to(".heading",{
-    transform:translate-X(-50%) ,
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".heading h1",{
+    x: "-133%",
+    ease:"none",
+    duration:3,
+    scrollTrigger:{
+        trigger:".heading",
+    start: "top 0%",
+    end: "bottom -10%",
+    scrub:2,
+    pin:true,
+markers:true,}
+
+})
+var cursor = document.querySelector(".cursor");
+window.addEventListener("mousemove", function(dets){
+    console.log(dets);
+    cursor.style.left = dets.x + "px";
+    cursor.style.top = dets.y + "px";
 })

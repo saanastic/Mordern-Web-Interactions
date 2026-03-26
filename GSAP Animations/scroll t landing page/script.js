0360e1a4132197c8821241006ea1c54995b05b86
Saanvi Arora <arorasaanvi777@gmail.com>
2026-03-26 23:00:44 +0530

    fimal scroll trigger fix

---
+++ GSAP Animations/scroll t landing page/script.js
@@ -0,0 +1,70 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.set(".bottle", { rotate: 25 ,
+    scale:1.2
+});
+
+
+gsap.to(".bottle", {
+    rotate:0,
+    scale:1,
+    ease: "none",
+    scrollTrigger :{
+        trigger: ".second",
+          start: "top 95%",  
+          end: "top 20%", 
+      scrub:true,
+    }
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".third",
+        start: "top 95%",
+        end: "top 10%",
+        scrub: true,
+    }
+})
+.to(".bottle", {
+    x: "30vw",
+    rotate: -15,
+    ease: "none"
+})
+.to(".bottle", {
+    x: "15vw",
+    scale: 0.8,
+    rotate: 0,
+    ease: "none"
+});
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".fourth",
+        start: "top 95%",
+        end: "top 10%",
+        scrub: true,
+    }
+})
+.to(".bottle", {
+    x: "-30vw",
+    rotate: 15,
+    scale: 1,
+    ease: "none"
+})
+.to(".bottle", {
+    x: "-23vw",
+    scale: 0.8,
+    rotate: 0,
+    ease: "none"
+});
+gsap.to(".bottle", {
+    scale: 0.1,
+    ease: "none",
+    x:"-45vw",
+    y:"9vh",
+    scrollTrigger: {
+        trigger: "footer",
+        start: "top 95%",
+        end: "top 40%",
+        scrub: true,
+        markers: true,
+    }
+})
